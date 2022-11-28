4b07edf33c78b7da015a9864135eb0724cf3119f
AndresParra <freddyandresparraorellana@yandex.com>
2022-11-28 12:08:14 +0300

    Commit corrected all issues on code about PEP8

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -88,20 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':'django.contrib.auth.password_validation.'
-        +'UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        + 'UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.'
-        +'MinimumLengthValidator',
+        + 'MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.'
-        +'CommonPasswordValidator',
+        + 'CommonPasswordValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.'
-        +'NumericPasswordValidator',
+        + 'NumericPasswordValidator',
     },
 ]
 
